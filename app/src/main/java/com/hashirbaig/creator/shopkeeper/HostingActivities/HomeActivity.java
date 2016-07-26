c4bed06bfd5b1b5d971dd6adcfa2230541e92d43
Hashir Baig <hashirbaig@gmail.com>
2016-07-27 01:57:38 +0500

    Improved Navigation Menu

--- app/src/main/java/com/hashirbaig/creator/shopkeeper/HostingActivities/HomeActivity.java
+++ app/src/main/java/com/hashirbaig/creator/shopkeeper/HostingActivities/HomeActivity.java
@@ -1,23 +1,28 @@
 package com.hashirbaig.creator.shopkeeper.HostingActivities;
 
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 import com.hashirbaig.creator.shopkeeper.MainFragments.CustomersListFragment;
+import com.hashirbaig.creator.shopkeeper.MainFragments.HomeFragment;
 import com.hashirbaig.creator.shopkeeper.R;
 
-public class MainActivity extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity{
 
     private NavigationView mNavigationView;
     private Toolbar mToolbar;
     private DrawerLayout mDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,9 +34,20 @@ public class MainActivity extends AppCompatActivity{
 
         setSupportActionBar(mToolbar);
         setupDrawerContent(mNavigationView);
+
+        mDrawerToggle = setupDrawerToggle();
+        mDrawerToggle.syncState();
+
+        Fragment fragment = HomeFragment.newInstance();
+        FragmentManager fm = getSupportFragmentManager();
+        fm.beginTransaction().add(R.id.fragment_container_drawer, fragment).commit();
     }
 
-    public void setupDrawerContent(NavigationView mNavView) {
+    private ActionBarDrawerToggle setupDrawerToggle(){
+        return new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.open_navigation_drawer, R.string.close_navigation_drawer);
+    }
+
+    private void setupDrawerContent(NavigationView mNavView) {
         mNavView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(MenuItem item) {
@@ -41,7 +57,7 @@ public class MainActivity extends AppCompatActivity{
         });
     }
 
-    public void onItemSelected(MenuItem item) {
+    private void onItemSelected(MenuItem item) {
 
         Fragment fragment = null;
         Class fragmentClass;
@@ -49,6 +65,9 @@ public class MainActivity extends AppCompatActivity{
         switch (item.getItemId()) {
             case R.id.customer_list_nav:
                 fragmentClass = CustomersListFragment.class;
+                break;
+            case R.id.home_nav:
+                fragmentClass = HomeFragment.class;
                 break;
             default:
                 fragmentClass = CustomersListFragment.class;
@@ -72,9 +91,23 @@ public class MainActivity extends AppCompatActivity{
     }
 
     @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
     protected void onPostCreate(@Nullable Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        if(mDrawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ app/src/main/java/com/hashirbaig/creator/shopkeeper/MainFragments/HomeFragment.java
@@ -0,0 +1,11 @@
+package com.hashirbaig.creator.shopkeeper.MainFragments;
+
+import android.support.v4.app.Fragment;
+
+public class HomeFragment extends Fragment{
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+}
